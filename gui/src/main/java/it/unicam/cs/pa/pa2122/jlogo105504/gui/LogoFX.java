12d5316ea65126026152ce6c08453a5f706fec21
Francesco Chiocchi <francesco.chiocchi@studenti.unicam.it>
2022-08-25 12:46:09 +0200

    Aggiornamento GUI

--- gui/src/main/java/it/unicam/cs/pa/pa2122/jlogo105504/gui/LogoFX.java
+++ gui/src/main/java/it/unicam/cs/pa/pa2122/jlogo105504/gui/LogoFX.java
@@ -2,21 +2,24 @@ package it.unicam.cs.pa.pa2122.jlogo105504.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class LogoFX extends Application {
 
-
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LogoFX.class.getResource("fxml/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("JLogo");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/home.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("JLogo");
+        primaryStage.getIcons().add(new Image(getClass().getResource("icons/Logo_icon.jpg").toString()));
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- gui/src/main/java/it/unicam/cs/pa/pa2122/jlogo105504/gui/controller/LogoController.java
+++ gui/src/main/java/it/unicam/cs/pa/pa2122/jlogo105504/gui/controller/LogoController.java
@@ -2,10 +2,19 @@ package it.unicam.cs.pa.pa2122.jlogo105504.gui.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+
+import java.awt.event.ActionEvent;
 
 public class LogoController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private MenuItem openFile;
+
+    @FXML
+    private MenuItem changeSizePanel;
 
     @FXML
     protected void onHelloButtonClick() {
